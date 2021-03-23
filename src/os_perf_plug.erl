52548be879dd1282a91cffb33e9cd2d7b9c7402c
Michal Slaski <michal.slaski@gmail.com>
2021-03-23 17:05:28 +0100

    Highlight Erlang functions in green

--- src/os_perf_plug.erl
+++ src/os_perf_plug.erl
@@ -1,10 +1,32 @@
 -module(os_perf_plug).
 
+-define(RESET_BG, <<"\e[49m">>).
+-define(RESET, <<"\e[0m">>).
+-define(GRAY_BG, <<"\e[7m">>).
+-define(YELLOW, <<"\e[33m">>).
+-define(RED, <<"\e[31m">>).
+-define(L_RED, <<"\e[48m">>).
+-define(GREEN, <<"\e[32;1m">>).
+-define(L_GREEN, <<"\e[92m">>).
+-define(CHOOSE_BG, <<"\e[42m">>).
+-define(RED_BG, <<"\e[48;2;184;0;0m">>).
+-define(L_GRAY_BG, <<"\e[48;2;80;80;80m">>).
+-define(UNDERLINE, <<"\e[4m">>).
+
 %% observer_cli_plugin Callback API
 -export([attributes/1, sheet_header/0, sheet_body/1]).
 
 attributes(PrevState) ->
-    {[], PrevState}.
+    Attrs =
+        [
+         %% [#{content => "$global::int_div_rem_body_shared", width => 100, color => ?L_GREEN}],
+         %% [#{content => "do_syscall_64", width => 100}],
+         %% [#{content => "entry_SYSCALL_64_after_hwframe", width => 100}],
+         %% [#{content => "epoll_wait", width => 100}],
+         %% [#{content => "1_scheduler", width => 100, color => ?L_RED}]
+        ],
+
+    {Attrs, PrevState}.
 
 sheet_header() ->
     [
@@ -13,6 +35,11 @@ sheet_header() ->
     ].
 
 sheet_body(PrevState) ->
+    SheetBody = get_stacks(),
+    {SheetBody, PrevState}.
+
+
+get_stacks() ->
     OSPid = os:getpid(),
     Cmd = io_lib:format("sudo /usr/share/bcc/tools/profile -F 4000 -f -p ~s 1", [OSPid]),
     Result = os:cmd(Cmd),
@@ -22,15 +49,26 @@ sheet_body(PrevState) ->
                     end,
                     #{},
                     lists:sort(lists:map(fun(Line) ->
-                                                 case string:tokens(Line, " ") of
-                                                     [Stack, Num] -> [string:tokens(Stack, ";"), list_to_integer(Num)];
-                                                     _ -> [Line, -1]
+                                                 case string:split(Line, " ", trailing) of
+                                                     [Stack, Num] ->
+                                                         case catch list_to_integer(Num) of
+                                                             NumInt when is_integer(NumInt) ->
+                                                                 [string:tokens(Stack, ";"), NumInt];
+                                                             _ ->
+                                                                 [["skipped"], -1]
+                                                         end;
+                                                     _ -> [[Line], -1]
                                                  end
                                          end,
                                          string:tokens(Result, "\n")
                                         )
                               )
                    ),
-    SheetBody = lists:map(fun({Stack, Num}) -> [Num, lists:last(Stack)] end, maps:to_list(M)),
+    lists:map(fun({Stack, Num}) ->
+                      TopFrame = lists:last(Stack),
+                      ColourTopFrame = case hd(TopFrame) of
+                                           $$ -> <<?GREEN/binary, (list_to_binary(TopFrame))/binary, ?RESET/binary>>;
+                                           _ -> TopFrame
+                                       end,
+                      [Num, ColourTopFrame] end, maps:to_list(M)).
 
-    {SheetBody, PrevState}.
